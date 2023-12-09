4e88a73e5924969537596213e73ffcb39dbfb3c4
Wells <12212108@mail.sustech.edu.cn>
2023-12-09 20:56:20 +0800

    实现了Datebase Service中的添加数据操作

--- sustc-api/src/main/java/io/sustc/service/impl/DatabaseServiceImpl.java
+++ sustc-api/src/main/java/io/sustc/service/impl/DatabaseServiceImpl.java
@@ -9,10 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.sql.DataSource;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.Arrays;
 import java.util.List;
 
@@ -36,8 +33,8 @@ public class DatabaseServiceImpl implements DatabaseService {
 
     @Override
     public List<Integer> getGroupMembers() {
-        throw new UnsupportedOperationException("TODO: replace this with your own student id");
-        // return Arrays.asList(12210000, 12210001, 12210002);
+//        throw new UnsupportedOperationException("TODO: replace this with your own student id");
+        return Arrays.asList(12212108);
     }
 
     @Override
@@ -46,8 +43,100 @@ public class DatabaseServiceImpl implements DatabaseService {
             List<UserRecord> userRecords,
             List<VideoRecord> videoRecords
     ) {
-        throw new UnsupportedOperationException("TODO: implement your import logic");
+        String sql_Danmu = "INSERT INTO \"DanmuRecord\"(Bv, Mid, Time, Content, PostTime, LikedBy, Danmu_is_Deleted) VALUES (?, ?, ?, ?, ?, ?, ?)";
+        String sql_User = "INSERT INTO \"UserRecord\"(Mid, Name, Sex, Birthday, Level, Coin, Sign, Following, Identity, Password, QQ, WeChat, User_is_Deleted) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+        String sql_Video = "INSERT INTO \"VideoRecord\"(Bv, Title, OwnerMid, OwnerName, CommitTime, ReviewTime, PublicTime, Duration, Description, Reviewer, \"Like\", Coin, Favorite, ViewerMids, ViewTime) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+
+        try (Connection connDanmu = dataSource.getConnection();
+             Connection connUser = dataSource.getConnection();
+             Connection connVideo = dataSource.getConnection();
+             PreparedStatement stmtDanmu = connDanmu.prepareStatement(sql_Danmu);
+             PreparedStatement stmtUser = connUser.prepareStatement(sql_User);
+             PreparedStatement stmtVideo = connVideo.prepareStatement(sql_Video)
+        ) {
+            // Insert Danmu Records
+            for (DanmuRecord danmuRecord : danmuRecords) {
+                stmtDanmu.setString(1, danmuRecord.getBv());
+                stmtDanmu.setLong(2, danmuRecord.getMid());
+                stmtDanmu.setFloat(3, danmuRecord.getTime());
+                stmtDanmu.setString(4, danmuRecord.getContent());
+                stmtDanmu.setTimestamp(5, danmuRecord.getPostTime());
+                Long[] likedByArray = Arrays.stream(danmuRecord.getLikedBy()).boxed().toArray(Long[]::new);
+                Array likedBySqlArray = connDanmu.createArrayOf("BIGINT", likedByArray);
+                stmtDanmu.setArray(6, likedBySqlArray);
+                stmtDanmu.setBoolean(7, danmuRecord.isDanmu_is_Deleted());
+                log.info("SQL: {}", stmtDanmu);
+                stmtDanmu.executeUpdate();
+                likedBySqlArray.free();
+            }
+
+            // Insert User Records
+            for (UserRecord userRecord : userRecords) {
+                stmtUser.setLong(1, userRecord.getMid());
+                stmtUser.setString(2, userRecord.getName());
+                stmtUser.setString(3, userRecord.getSex());
+                stmtUser.setString(4, userRecord.getBirthday());
+                stmtUser.setShort(5, userRecord.getLevel());
+                stmtUser.setInt(6, userRecord.getCoin());
+                stmtUser.setString(7, userRecord.getSign());
+                stmtUser.setString(8, userRecord.getIdentity().name());
+                stmtUser.setString(9, userRecord.getPassword());
+                stmtUser.setString(10, userRecord.getQq());
+                stmtUser.setString(11, userRecord.getWechat());
+                Long[] followingArray = Arrays.stream(userRecord.getFollowing()).boxed().toArray(Long[]::new);
+                Array followingSqlArray = connUser.createArrayOf("BIGINT", followingArray);
+                stmtUser.setArray(12, followingSqlArray);
+                stmtUser.setBoolean(13, userRecord.isUser_is_Deleted());
+                log.info("SQL: {}", stmtUser);
+                stmtUser.executeUpdate();
+                followingSqlArray.free();
+            }
+
+            // Insert Video Records
+            for (VideoRecord videoRecord : videoRecords) {
+                stmtVideo.setString(1, videoRecord.getBv());
+                stmtVideo.setString(2, videoRecord.getTitle());
+                stmtVideo.setLong(3, videoRecord.getOwnerMid());
+                stmtVideo.setString(4, videoRecord.getOwnerName());
+                stmtVideo.setTimestamp(5, videoRecord.getCommitTime());
+                stmtVideo.setTimestamp(6, videoRecord.getReviewTime());
+                stmtVideo.setTimestamp(7, videoRecord.getPublicTime());
+                stmtVideo.setFloat(8, videoRecord.getDuration());
+                stmtVideo.setString(9, videoRecord.getDescription());
+                stmtVideo.setLong(10, videoRecord.getReviewer());
+                Long[] likeArray = Arrays.stream(videoRecord.getLike()).boxed().toArray(Long[]::new);
+                Array likeSqlArray = connVideo.createArrayOf("BIGINT", likeArray);
+                stmtVideo.setArray(11, likeSqlArray);
+                Long[] coinArray = Arrays.stream(videoRecord.getCoin()).boxed().toArray(Long[]::new);
+                Array coinSqlArray = connVideo.createArrayOf("BIGINT", coinArray);
+                stmtVideo.setArray(12, coinSqlArray);
+                Long[] favoriteArray = Arrays.stream(videoRecord.getFavorite()).boxed().toArray(Long[]::new);
+                Array favoriteSqlArray = connVideo.createArrayOf("BIGINT", favoriteArray);
+                stmtVideo.setArray(13, favoriteSqlArray);
+                Long[] viewerMidsArray = Arrays.stream(videoRecord.getViewerMids()).boxed().toArray(Long[]::new);
+                Array viewerMidsSqlArray = connVideo.createArrayOf("BIGINT", viewerMidsArray);
+                stmtVideo.setArray(14, viewerMidsSqlArray);
+                // Convert float array to Double array
+                Double[] viewTimeArray = new Double[videoRecord.getViewTime().length];
+                for (int i = 0; i < viewTimeArray.length; i++) {
+                    viewTimeArray[i] = (double) videoRecord.getViewTime()[i];
+                }
+// Use the Double array in the SQL statement
+                stmtVideo.setArray(15, connVideo.createArrayOf("FLOAT8", viewTimeArray));
+                log.info("SQL: {}", stmtVideo);
+                stmtVideo.executeUpdate();
+                likeSqlArray.free();
+                coinSqlArray.free();
+                favoriteSqlArray.free();
+                viewerMidsSqlArray.free();
+            }
+        } catch (SQLException ex) {
+            throw new RuntimeException(ex);
+        }
     }
+
+
+
 
     /*
      * The following code is just a quick example of using jdbc datasource.
